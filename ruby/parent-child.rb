f592917c704e5b36374fb676da04c686db146b61
Tony's Ubuntu <tonyhsieh@onead.com.tw>
2015-12-01 15:29:50 +0800

    update gurad file and spec file path

--- ruby/parent-child.rb
+++ ruby/parent-child.rb
@@ -1,15 +1,29 @@
 def parent_child data
   roots = []
-  tmp_hash = data.map do |d| d[:children] = []; {:id => d[:id], :value => d} }
-  data.each do |d|
+
+  tmp_hash = []
+
+  data.each_with_index do |d, i|
+
+    tmp_hash[d[:id]] = {
+      :id => d[:id],
+      :children=> []
+    }
+
+    node = tmp_hash[d[:id]]
     if d[:parent].to_i === 0
-      roots.push d
+      roots.push node
     else
-      parent = tmp_hash[d[:id]][:value]
-      puts "!!!!!!!!!!!!"
-      puts parent
-      parent[:children].push d
+      parent = tmp_hash[d[:parent]]
+
+      # puts parent
+      parent[:children].push node
     end
+    puts i.to_s + " roots: "
+    puts roots
+    # i = i + 1
   end
+  # puts "!!!!!!!!!!!!"
+
   return roots
 end
