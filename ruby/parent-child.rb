fc49758291238d47be946c3ec48ea77d76a851d5
Tony's Ubuntu <tonyhsieh@onead.com.tw>
2015-12-01 15:57:50 +0800

    improve ruby inplement

--- ruby/parent-child.rb
+++ ruby/parent-child.rb
@@ -1,29 +1,25 @@
 def parent_child data
   roots = []
-
   tmp_hash = []
 
-  data.each_with_index do |d, i|
-
+  data.each do |d|
     tmp_hash[d[:id]] = {
       :id => d[:id],
       :children=> []
     }
+  end
 
+  data.each_with_index do |d, i|
     node = tmp_hash[d[:id]]
     if d[:parent].to_i === 0
       roots.push node
     else
       parent = tmp_hash[d[:parent]]
-
-      # puts parent
       parent[:children].push node
     end
     puts i.to_s + " roots: "
     puts roots
-    # i = i + 1
   end
-  # puts "!!!!!!!!!!!!"
 
   return roots
 end
--- ruby/parent-child_spec.rb
+++ ruby/parent-child_spec.rb
@@ -5,15 +5,20 @@ describe "parent child should be..." do
 
     # root nodes index must less than other childs
     @data = [
-      {:id => 1, :parent => 0},
-      {:id => 2, :parent => 0},
       {:id => 4, :parent => 2},
       {:id => 3, :parent => 1},
+      {:id => 5, :parent => 3},
+      {:id => 1, :parent => 0},
+      {:id => 2, :parent => 0},
     ]
 
     expect_result = [
       {:id => 1, :children => [
-        {:id => 3, :children => []}
+        {
+          :id => 3, :children => [
+            {:id => 5, :children => []}
+          ]
+        }
       ]},
       {:id => 2, :children => [
         {:id => 4, :children => []}
