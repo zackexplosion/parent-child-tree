c9dd90c391f835d9593b939560c282cc1bfc5e22
Tony's Ubuntu <tonyhsieh@onead.com.tw>
2015-12-01 14:32:19 +0800

    improve spec

--- ruby/parent-child.rb
+++ ruby/parent-child.rb
@@ -1,3 +1,15 @@
 def parent_child data
-	data
+  roots = []
+  tmp_hash = data.map do |d| d[:children] = []; {:id => d[:id], :value => d} }
+  data.each do |d|
+    if d[:parent].to_i === 0
+      roots.push d
+    else
+      parent = tmp_hash[d[:id]][:value]
+      puts "!!!!!!!!!!!!"
+      puts parent
+      parent[:children].push d
+    end
+  end
+  return roots
 end
--- ruby/spec/parent-child_spec.rb
+++ ruby/spec/parent-child_spec.rb
@@ -1,25 +1,25 @@
 require_relative '../parent-child.rb'
 
 describe "parent child should be..." do
-  before(:each) do
-  	@data = [
-  		{:id => 1, :parent => 0},
-  		{:id => 2, :parent => 0},
-  		{:id => 3, :parent => 1},
-  		{:id => 4, :parent => 2},
-  	]
-  end
-  it "should be true" do
-  	expect_result = [
-  		{:id => 1, :parent => 0 , :children => [
-  			{:id => 3, :parent => 1}
-  		]},
-  		{:id => 2, :parent => 0 , :children => [
-  			{:id => 4, :parent => 2}
-  		]},
-  	]
-  	result = parent_child @data
-  	puts result
+  it "should be eaul" do
+    @data = [
+      {:id => 1, :parent => 0},
+      {:id => 2, :parent => 0},
+      {:id => 3, :parent => 1},
+      {:id => 4, :parent => 2},
+    ]
+
+    expect_result = [
+      {:id => 1, :parent => 0 , :children => [
+        {:id => 3, :parent => 1, :children => []}
+      ]},
+      {:id => 2, :parent => 0 , :children => [
+        {:id => 4, :parent => 2, :children => []}
+      ]},
+    ]
+
+    result = parent_child @data
+    puts result
     expect(result).to eq(expect_result)
   end
 end
